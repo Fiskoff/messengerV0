466eda6b06a8711c820c573f01bffc42b7fc3e20
Fiskoff <fiskoff2015@yandex.ru>
2025-06-24 22:03:40 +0700

    modified "create_user_endpoint"

--- fastAPI_application/app/api/__init__.py
+++ fastAPI_application/app/api/__init__.py
@@ -2,4 +2,4 @@ from fastapi import APIRouter
 from app.api.routers.user_router import auth_router
 
 main_router = APIRouter()
-main_router.include_router(auth_router, prefix="/auth")
+main_router.include_router(auth_router, prefix="/auth", tags=["Auth"])
