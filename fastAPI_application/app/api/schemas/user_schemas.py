eb0816b5140fbc7b6777a9f39cd6ca366afe7a38
Fiskoff <fiskoff2015@yandex.ru>
2025-07-17 20:03:06 +0700

    fix

--- fastAPI_application/app/api/schemas/user_schemas.py
+++ fastAPI_application/app/api/schemas/user_schemas.py
@@ -6,19 +6,6 @@ class UserSchema(BaseModel):
     login: str
 
 
-class UserCreate(UserSchema):
-    password: str
-
-    class Config:
-        json_schema_extra = {
-            "example": {
-                "name": "Sergey123 | None",
-                "login": "fiskoff123",
-                "password": "root123",
-            }
-        }
-
-
 class UserRead(UserSchema):
     id: int
 
@@ -28,5 +15,20 @@ class UserRead(UserSchema):
                 "name": "Sergey123",
                 "login": "fiskoff123",
                 "id": 123,
+            }
+        }
+
+
+class UserCreate(BaseModel):
+    name: str | None
+    login: str
+    password: str
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "name": "Sergey123 | None",
+                "login": "fiskoff123",
+                "password": "root123",
             }
         }
