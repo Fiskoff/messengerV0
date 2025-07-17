2596832b86b5fd301b48d69e28495ba74b8ac98d
Fiskoff <fiskoff2015@yandex.ru>
2025-07-17 19:40:10 +0700

    delete TokenData, TokenResponse

--- fastAPI_application/app/api/schemas/token_schemas.py
+++ fastAPI_application/app/api/schemas/token_schemas.py
@@ -5,11 +5,3 @@ class Token(BaseModel):
     access_token: str
     refresh_token: str
     token_type: str = "bearer"
-
-
-class TokenData(BaseModel):
-    login: str | None = None
-
-
-class TokenResponse(Token):
-    pass
