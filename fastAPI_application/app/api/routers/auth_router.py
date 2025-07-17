34dd132ebe55417b08f3dc10c4a4c1a905c36294
Fiskoff <fiskoff2015@yandex.ru>
2025-07-17 19:40:46 +0700

    Created login_user, refresh_access_token

--- fastAPI_application/app/api/routers/auth_router.py
+++ fastAPI_application/app/api/routers/auth_router.py
@@ -1,7 +1,10 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 
+from app.api.schemas.token_schemas import Token
 from app.api.schemas.user_schemas import UserRead, UserCreate
-from app.services.auth_services import Registration
+from app.services.auth_services import Registration, Authorization
+from app.services.create_jwt import CreateJWT
 
 auth_router = APIRouter()
 
@@ -13,13 +16,36 @@ async def register_user(user_data: UserCreate) -> UserRead | dict:
 
 
 @auth_router.post("/login")
-async def login_user():
-    pass
+async def login_user(form_data: OAuth2PasswordRequestForm = Depends()):
+    auth_service = Authorization(login=form_data.username,password=form_data.password)
+    tokens = await auth_service.authorization()
+    if not tokens:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return tokens
 
 
-@auth_router.post("/refresh")
-async def get_access_token():
-    pass
+@auth_router.post("/refresh", response_model=Token)
+async def refresh_access_token(token: Token):
+    jwt_creator = CreateJWT()
+    payload = jwt_creator.verify_token(token.refresh_token)
+
+    if not payload or payload.get("token_type") != "refresh":
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid refresh token"
+        )
+
+    new_access_token = jwt_creator.create_access_token({"sub": payload["sub"]})
+    new_refresh_token = jwt_creator.create_refresh_token({"sub": payload["sub"]})
+    return Token(
+        access_token=new_access_token,
+        refresh_token=new_refresh_token,
+        token_type="bearer"
+    )
 
 
 @auth_router.post("/logout")
